52ff4b360e0ceb6db6c53d51f62762a8a3e2dd57
jojo3420 <jjjhhhvvv@gmail.com>
2021-08-17 17:54:05 +0900

    todo app  - Create, Delete, Read All

--- my_todo_app/urls.py
+++ my_todo_app/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('newTodo/', views.new_todo, name='new_todo'),
+    path('deleteTodo/', views.delete_todo, name='delete_todo')
 ]
--- my_todo_app/views.py
+++ my_todo_app/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse('hello world')
-    return render(request, 'my_todo_app/index.html')
+    todos = Todo.objects.all()
+    data = {'todos': todos}
+    return render(request, 'my_todo_app/index.html', data)
+
+
+def new_todo(request):
+    content = request.POST['todo_content']
+    # print(content)
+    todo = Todo(content=content)
+    todo.save()
+    # return HttpResponse('신규 메모 작성: ' + content)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete_todo(request):
+    id = request.GET['id']
+    # print(f'id: {id}')
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
